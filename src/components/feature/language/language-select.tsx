ea465a3c1ee24cf2b50ae8619f87266bf013eae6
Dmytro <dmytropolizhai@gmail.com>
2026-01-02 14:30:31 +0200

    refactor(language-select): change structure of the component, improve loading system
    Signed-off-by: Dmytro <dmytropolizhai@gmail.com>

--- src/components/feature/language/language-select.tsx
+++ src/components/feature/language/language-select.tsx
@@ -1,5 +1,5 @@
 import {
-    Option,
+    type Option,
     Select,
     SelectContent,
     SelectGroup,
@@ -9,34 +9,41 @@ import {
     SelectValue,
 } from "@/components/ui/select"
 import { useStorage } from "@/hooks/use-storage"
-import { LanguageList } from "./lib/language-list"
+import { LanguageOptionList } from "./lib/language-option-list"
 import { useTranslation } from "react-i18next"
-import { Language } from '@/types/language';
 import { useEffect } from 'react';
+import { Language, isLanguage } from '@/types/language';
+import { getOptionByLanguage } from '@/components/feature/language/lib/utils';
 
 
 export const LanguageSelect = () => {
     const { t, i18n } = useTranslation("common", { keyPrefix: "screen.settings.language" });
-    const [language, setLanguage] = useStorage<Language>("lang");
+    const [language, setLanguage] = useStorage<Language>("lang", "en");
 
     useEffect(() => {
+      if (!language) return;
       i18n.changeLanguage(language);
-    }, [])
+    }, [language]);
 
     function handleOnValueChange(option: Option) {
-        i18n.changeLanguage(option?.value);
-        setLanguage(option?.value as Language);
+      if (!option) return;
+
+      const { value } = option;
+      if (isLanguage(value)) {
+        i18n.changeLanguage(value);
+        setLanguage(value);
+      }
     }
 
     return (
-        <Select value={{ label: language!, value: language! }} onValueChange={handleOnValueChange} >
+        <Select value={getOptionByLanguage(language!)} onValueChange={handleOnValueChange} >
             <SelectTrigger>
                 <SelectValue placeholder={t("placeholder")} />
             </SelectTrigger>
             <SelectContent >
                 <SelectGroup>
                     <SelectLabel>{t("title")}</SelectLabel>
-                    {LanguageList.map(lang => (
+                    {LanguageOptionList.map(lang => (
                         <SelectItem key={lang.value} label={lang.label} value={lang.value}>
                             {lang.label}
                         </SelectItem>
