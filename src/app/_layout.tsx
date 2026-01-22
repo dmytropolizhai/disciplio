15d131ef538b828515b75849644341db5d7d743c
Dmytro <dmytropolizhai@gmail.com>
2026-01-22 11:01:47 +0200

    fix(layout): remove migrations import

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -8,18 +8,12 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { Suspense } from 'react';
-import { openDatabaseSync, SQLiteProvider } from 'expo-sqlite';
+import { SQLiteProvider } from 'expo-sqlite';
 import { ActivityIndicator } from 'react-native';
 import { DATABASE_NAME } from '@/database/schema';
-import migrations from '@/database/drizzle/migrations';
-import { drizzle } from 'drizzle-orm/expo-sqlite';
-import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
 
 export default function RootLayout() {
   const { colorScheme } = useColorScheme();
-  const expoDb = openDatabaseSync(DATABASE_NAME);
-  const db = drizzle(expoDb);
-  const { success, error } = useMigrations(db, migrations);
 
   return (
     <Suspense fallback={<ActivityIndicator size="large" />} >
