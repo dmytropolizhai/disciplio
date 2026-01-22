c8678dc9377a1f6196bd2c19fcabee627815a7dc
Dmytro <dmytropolizhai@gmail.com>
2026-01-22 10:02:53 +0200

    refactor: change "tasks" to "task"

--- src/components/feature/todo/todo-controls.tsx
+++ src/components/feature/todo/todo-controls.tsx
@@ -1,12 +1,11 @@
 import { View } from "react-native"
-import { CreateTaskButton } from "../tasks/create-task-button"
+import { CreateTaskButton } from "@/components/feature/task/create-task-button"
 
 export const TodoControls = () => {
 
     return (
         <View className="w-full p-4 h-25">
             <CreateTaskButton />
-
         </View>
     )
 }
